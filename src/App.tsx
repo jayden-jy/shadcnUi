32c6fd41bc40608cd2c7d0363ce98aa3f80488e2
jayden-jy <markup.jy@gmail.com>
2026-06-24 15:03:04 +0900

    useState란 무엇인가?

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,47 @@
+import { useState } from "react";
+
 function App() {
-  return <div>App.tsx 파일입니다.</div>;
+  // useState => Hooks
+  // useState는 리액트에서 가장 기본적인 훅(Hook)이며, 함수 컴포넌트에서도 가변적인 상태를 지닐 수 있게 해준다.
+  // => 이 함수가 호출되면 배열을 반환한다.
+  // => 변환된 배열의 첫 번째 요소는 상태 값, 두 번째 요소는 상태 값을 설정하는 함수
+  // => useState 함수의 파라미터(매개변수)에는 상태의 기본값, 초기값을 넣어 줍니다.
+
+  const [value, setValue] = useState<number>(0);
+  const [name, setName] = useState<string>(
+    "빈 문자열로 할당하지 않은 name 상태 값입니다.",
+  );
+  const [nickname, setNickname] = useState<string>("");
+
+  const increment = () => setValue(value + 1);
+  const decrement = () => setValue(value - 1);
+
+  const onChangeName = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setName(event.target.value);
+  };
+  const onChangeNickname = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setNickname(event.target.value);
+  };
+
+  return (
+    <div>
+      <p>
+        현재 타운터 값은: <b>{value}</b>
+      </p>
+      <button onClick={increment}>1 증가</button>
+      <button onClick={decrement}>1 감소</button>
+
+      <div>
+        <input type="text" value={name} onChange={onChangeName} />
+        <input type="text" onChange={onChangeNickname} />
+      </div>
+
+      <div>
+        <b>이름 : {name}</b>
+        <b>별명 : {nickname}</b>
+      </div>
+    </div>
+  );
 }
 
 export default App;
