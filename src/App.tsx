c559ea86172144044c5dc205dce821f8186a7d6d
jayden-jy <markup.jy@gmail.com>
2026-06-27 18:56:56 +0900

    Side-bar 컴포넌트

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { AppFooter, AppHeader } from "./components/common";
+import { AppFooter, AppHeader, AppSidebar } from "./components/common";
 import { ThemeProvider } from "./components/theme-provider";
 import HookDashboard from "./pages/HookDashboard";
 
@@ -8,7 +8,14 @@ function App() {
       <div className="page">
         <AppHeader />
         <div className="container">
-          <HookDashboard />
+          <main className="w-full h-full min-h-[720px] flex p-6 gap-6">
+            {/* 카테고리 사이드바 */}
+            <AppSidebar />
+            {/* 컨텐츠 */}
+            <section>
+              <HookDashboard />
+            </section>
+          </main>
         </div>
         <AppFooter />
       </div>
