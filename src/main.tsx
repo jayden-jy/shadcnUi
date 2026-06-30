97a8b1a19112ca29543a6f4bdf05b1d7cbab22f2
jayden-jy <markup.jy@gmail.com>
2026-06-30 10:46:44 +0900

    페이지 라우팅 및 outlet을 활용한 전역 레이아웃 관리

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router";
+import { ThemeProvider } from "./components/theme-provider.tsx";
 
-createRoot(document.getElementById('root')!).render(
+import RootLayout from "./layout.tsx"; // 전역 레이아웃 컴포넌트
+import App from "./pages"; // 메인 페이지
+import SingUp from "./pages/sign-up"; // 회원가입 페이지
+import SingIn from "./pages/sign-in"; // 로그인 페이지
+import "./index.css";
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <BrowserRouter>
+        <Routes>
+          <Route element={<RootLayout />}>
+            <Route index element={<App />} />
+            <Route path="sign-up" element={<SingUp />} />
+            <Route path="sign-in" element={<SingIn />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   </StrictMode>,
-)
+);
---
+++ src/pages/index.tsx
@@ -0,0 +1,77 @@
+import { PencilLine } from "lucide-react";
+import { AppSidebar } from "../components/common";
+import {
+  SkeletonHotTopick,
+  SkeletonNewTopic,
+} from "../components/common/skeleton";
+import { Button } from "../components/ui";
+import HookDashboard from "./HookDashboard";
+
+function App() {
+  return (
+    <main className="w-full h-full min-h-[720px] flex p-6 gap-6">
+      <div className="fixed right-1/2 bottom-10 translate-x-1/2 z-20 items-center">
+        <Button
+          variant={"destructive"}
+          className="flex items-center gap-2 !py-5 !px-6 rounded-full"
+        >
+          <PencilLine className="w-4 h-4" />
+          <span>나만의 토픽 작성</span>
+        </Button>
+      </div>
+      {/* 카테고리 사이드바 */}
+      <AppSidebar />
+      {/* 컨텐츠 */}
+      <section className="flex flex-1 flex-col gap-12">
+        <HookDashboard />
+        {/* 핫 토픽 */}
+        <div className="w-full flex flex-col gap-4">
+          <div className="flex flex-col gap-1">
+            <div className="flex items-center gap-2">
+              <img src="/assets/gifs/fire.gif" alt="@IMG" className="w-7 h-7" />
+              <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
+                HOT 토픽
+              </h4>
+            </div>
+            <p className="md:text-base text-muted-foreground">
+              지금 가장 주목받는 주제들을 살펴보고, 다양한 관점의 인사이트를
+              얻어보세요.
+            </p>
+          </div>
+          <div className="grid grid-cols-4 gap-6">
+            <SkeletonHotTopick />
+            <SkeletonHotTopick />
+            <SkeletonHotTopick />
+            <SkeletonHotTopick />
+          </div>
+        </div>
+        {/* 뉴 토픽 */}
+        <div className="w-full flex flex-col gap-4">
+          <div className="flex flex-col gap-1">
+            <div className="flex items-center gap-2">
+              <img
+                src="/assets/gifs/writing-hand.gif"
+                alt="@IMG"
+                className="w-7 h-7"
+              />
+              <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
+                NEW 토픽
+              </h4>
+            </div>
+            <p className="md:text-base text-muted-foreground">
+              새로운 시선으로, 새로운 이야기를 시작해보세요.
+            </p>
+          </div>
+          <div className="grid grid-cols-2 gap-6">
+            <SkeletonNewTopic />
+            <SkeletonNewTopic />
+            <SkeletonNewTopic />
+            <SkeletonNewTopic />
+          </div>
+        </div>
+      </section>
+    </main>
+  );
+}
+
+export default App;
